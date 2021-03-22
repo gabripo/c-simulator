2c61db277608ea0f9eda2eb79bf0b8ae2d64bc8f
gabripo <gabripo93+github@gmail.com>
2021-03-22 18:49:35 +0100

    Structure assignment with C-syntax

--- simulation/ode_solver_test.c
+++ simulation/ode_solver_test.c
@@ -2,34 +2,37 @@
 
 int main(int argc, char const *argv[])
 {
-    odeSys sys;
-    finiteDifferenceMethod fwdEuler;
-    algSys toSolveEqs;
 
-    sys.nEqs = 1;
-    sys.odeEqs = "DxDt - a * x";
-    sys.odeICs = (int*) 0;
+    // TODO : Same way to create 2D char arrays of parametersSub for odeEqs
+    odeSys sys = {
+        .nEqs = 1,
+        .odeEqs = "DxDt - a * x",
+        .odeICs = (int*) 0,
+        .nSubs = 0,
+        .parametersSub = calloc(sys.nSubs, sizeof *sys.parametersSub),
+        .independentVariable = 't',
+        .derivativeIdentifier = 'D',
+    };
 
-    sys.nSubs = 0;
-    sys.parametersSub = calloc(sys.nSubs, sizeof *sys.parametersSub);
     //sys.parametersSub[0] = calloc(strlen("a = 1") + 1, sizeof **sys.parametersSub);
     //strncpy(sys.parametersSub[0], "a = 1", strlen("a = 1"));
     setParameterSubValues(&sys, "a = 1");
     setParameterSubValues(&sys, "b = 2");
     setParameterSubValues(&sys, "c = 3");
 
-    sys.independentVariable = 't';
-    sys.derivativeIdentifier = 'D';
+    finiteDifferenceMethod fwdEuler = {
+        .derivativeSub = "DxDt = ( x(k) - x(k-1) ) / h",
+        .timeStepName = 'h',
+        .timeStep = 0.1,
+        .abscissaIdentifier = 'k',
+        .independentVariable = sys.independentVariable,
+        .derivativeIdentifier = sys.derivativeIdentifier,
+    };
 
-    fwdEuler.derivativeSub = "DxDt = ( x(k) - x(k-1) ) / h";
-    fwdEuler.timeStepName = 'h';
-    fwdEuler.timeStep = 0.1;
-    fwdEuler.abscissaIdentifier = 'k';
-    fwdEuler.independentVariable = sys.independentVariable;
-    fwdEuler.derivativeIdentifier = sys.derivativeIdentifier;
-
-    toSolveEqs.nEqs = 1;
-    toSolveEqs.algEqs = (double*) 1;
+    algSys toSolveEqs = {
+        .nEqs = 1,
+        .algEqs = (double*) 1,
+    };
     
     odeNumericalConverter(sys, fwdEuler, &toSolveEqs);
     return 0;
